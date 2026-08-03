6f1797f82c3423b468e5f1f47cf60e8a9991e9d6
Alexander Kirichenko <aakirichenko@google.com>
2026-08-03 21:32:50 +0500

    misterCodeSync: fix warning (15074), pll_ntsc.mif not found

--- rtl/pll.v
+++ rtl/pll.v
@@ -185,7 +185,7 @@ module pll (
 		altpll_component.port_extclk3 = "PORT_UNUSED",
 		altpll_component.self_reset_on_loss_lock = "OFF",
 		altpll_component.width_clock = 5,
-		altpll_component.scan_chain_mif_file = "pll_ntsc.mif";
+		altpll_component.scan_chain_mif_file = "rtl/pll_ntsc.mif";
 
 
 endmodule
@@ -265,7 +265,7 @@ endmodule
 // Retrieval info: PRIVATE: PLL_PFDENA_CHECK STRING "0"
 // Retrieval info: PRIVATE: PLL_TARGET_HARCOPY_CHECK NUMERIC "0"
 // Retrieval info: PRIVATE: PRIMARY_CLK_COMBO STRING "inclk0"
-// Retrieval info: PRIVATE: RECONFIG_FILE STRING "pll_ntsc.mif"
+// Retrieval info: PRIVATE: RECONFIG_FILE STRING "rtl/pll_ntsc.mif"
 // Retrieval info: PRIVATE: SACN_INPUTS_CHECK STRING "1"
 // Retrieval info: PRIVATE: SCAN_FEATURE_ENABLED STRING "1"
 // Retrieval info: PRIVATE: SELF_RESET_LOCK_LOSS STRING "0"
@@ -346,7 +346,7 @@ endmodule
 // Retrieval info: CONSTANT: PORT_extclk3 STRING "PORT_UNUSED"
 // Retrieval info: CONSTANT: SELF_RESET_ON_LOSS_LOCK STRING "OFF"
 // Retrieval info: CONSTANT: WIDTH_CLOCK NUMERIC "5"
-// Retrieval info: CONSTANT: scan_chain_mif_file STRING "pll_ntsc.mif"
+// Retrieval info: CONSTANT: scan_chain_mif_file STRING "rtl/pll_ntsc.mif"
 // Retrieval info: USED_PORT: @clk 0 0 5 0 OUTPUT_CLK_EXT VCC "@clk[4..0]"
 // Retrieval info: USED_PORT: areset 0 0 0 0 INPUT GND "areset"
 // Retrieval info: USED_PORT: c0 0 0 0 0 OUTPUT_CLK_EXT VCC "c0"
